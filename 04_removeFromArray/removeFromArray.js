c7d1ee9d80bb639d33b622d9954886cfef97f6be
Yariv <FroznKnight@gmail.com>
2022-12-18 21:01:23 +0200

    Solved exercises 01 - 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr = [], ...anyArgs) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < anyArgs.length; j++) {
+      if (arr[i] === anyArgs[j]) {
+        arr.splice(i, 1);
+        i = 0;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let tempNum = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  ) {
+    return "ERROR";
+  } else {
+    if (num1 > num2) {
+      tempNum += num1;
+      num1 = 0;
+      num1 += num2;
+      num2 = 0;
+      num2 += tempNum;
+    }
+    for (let i = 0; i < num2; i++) {
+      sum += i + 1;
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
